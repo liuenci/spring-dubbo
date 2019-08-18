24ecd9d732c8a9fd859124ec09fd869fa3550884
liuenci <154910381@qq.com>
2019-08-18 12:15:22 +0800

    完成一次服务调用

--- consumer/src/main/java/com/liuencier/dubbo/ConsumerClient.java
+++ consumer/src/main/java/com/liuencier/dubbo/ConsumerClient.java
@@ -1,4 +1,20 @@
 package com.liuencier.dubbo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ConsumerClient {
+
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-hello.consumer.xml");
+        context.start();;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String message = scanner.next();
+            ServiceAPI serviceAPI = (ServiceAPI) context.getBean("consumerService");
+            System.out.println(serviceAPI.sendMessage(message));
+        }
+    }
 }
